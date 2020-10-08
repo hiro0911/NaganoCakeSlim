77cef7ac2966cf69ef25cd633764ed86f21757ad
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-10-08 19:20:29 +0000

    [Add]order注文ページ作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,26 @@
 class OrdersController < ApplicationController
-
+	def new
+		@order = Order.new
+		@deliveries = Delivery.where(customer_id: current_customer.id)
+	end
+	def confirm
+		@cart_items = CartItem.where(customer_id: current_customer.id)
+		@order = Order.new(customer_id: current_customer.id,
+											 payment: params[:payment],
+											 postage: 800)
+		if params[:addresses] == "ご自身の住所"
+			@order.postcode = current_customer.postcode
+			@order.address = current_customer.address
+			@order.name = current_customer.full_name_kanji
+		elsif params[:addresses] == "登録住所から選択"
+			@delivery = Delivery.find(params[:delivery_id])
+			@order.postcode = @delivery.postcode
+			@order.address = @delivery.address
+			@order.name = @delivery.name
+		elsif params[:addresses] == "新しいお届け先"
+			@order.postcode = params[:postcode]
+			@order.address = params[:address]
+			@order.name = params[:name]
+		end
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,4 @@
 class Order < ApplicationRecord
 	has_many :ordered_products, dependent: :destroy
 	belongs_to :customer
-
-	def total_price
-		order.billing_amount - order.postage
-	end
 end
