5a5c9f0ff998bc8aae1ff13b0877d1425cb7206b
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-10-10 19:21:05 +0000

    [Add]customerページ作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,28 @@
 class CustomersController < ApplicationController
 	def show
+		@customer = current_customer
+	end
+	def edit
+		@customer = current_customer
+	end
+	def withdrow
+	end
+	def switch
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		reset_session
+		redirect_to root_path, notice: "ご利用ありがとうございました。"
+	end
+	def customer_params
+		params.require(:customer).permit(:email,
+																     :password,
+													           :family_name_kanji, 
+													           :family_name_kana,
+													           :first_name_kanji,
+													           :first_name_kana,
+													           :postcode,
+													           :address,
+													           :tel,
+													           :admission_status)
 	end
 end
